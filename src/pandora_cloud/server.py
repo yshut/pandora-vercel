c36b28be80a8d77479d9cd322aae59242d0c4eed
yshut <93298228+yshut@users.noreply.github.com>
2023-05-01 13:23:25 +0800

    server.py

--- src/pandora_cloud/server.py
+++ src/pandora_cloud/server.py
@@ -25,7 +25,7 @@ class ChatBot:
         self.sentry = sentry
         self.login_local = login_local
         self.log_level = logging.DEBUG if debug else logging.WARN
-        self.api_prefix = getenv('CHATGPT_API_PREFIX', 'https://ai.fakeopen.com')
+        self.api_prefix = getenv('CHATGPT_API_PREFIX', '')
 
         hook_logging(level=self.log_level, format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s')
         self.logger = logging.getLogger('waitress')
